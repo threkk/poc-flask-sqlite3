a2c8245acf1038479731086978a33aea26f16967
Alberto Martinez de Murga Ramirez <alberto.martinez-de-murga-ramire@capgemini.com>
2019-02-07 15:19:22 +0100

    feat: DB connection.

--- poc/app.py
+++ poc/app.py
@@ -1,13 +1,41 @@
+"""
+Database connections in microservices usually attempt to initialise a
+a connection, keep it alive, and use it in the application. This a bit
+different when using Flask and SQLite.
+
+Python and Flask have a totally different approach in terms of threading and
+databases:
+
+- Flask uses thread-local objects internally so that you don’t have to pass
+  objects around from function to function within a request in order to stay
+  threadsafe.
+- sqlite3, the default module for SQLite, does not support multithreading.
+  Older SQLite versions had issues with sharing connections between threads.
+  That’s why the Python module disallows sharing connections and cursors
+  between threads. If you still try to do so, you will get an exception at
+  runtime.
+
+It is possible to disable the threading, using the option `check_same_thread`
+to False. However, the sqlite3 module is not threadsafe and Flask also does not
+get along with it so well.
+
+We will enable the write-ahead mode in SQLite and we will initalise a different
+connection per thread that we will hold in a global object in the thread so it
+can be reused in the same thread.
+"""
 from flask import Flask
 from poc.db import init_db, get_db
 
+# Initialise our app.
 app = Flask(__name__)
 
+# Initialise the database module. We will register it in the context so it is
+# available globally for the thread.
 init_db(app)
 
 
 @app.route('/')
 def index():
     db = get_db()
-    query = db.cursor().execute('SELECT 1')
-    return query
+    c = db.cursor()
+    return str(next(c.execute('SELECT 1'))[0])
--- poc/db.py
+++ poc/db.py
@@ -1,26 +1,59 @@
 import sqlite3
+import click
 
-from flask import g
-from os.path import join
+from flask import g, current_app
+from flask.cli import with_appcontext
+from os import environ
+from os.path import dirname, exists, join
 
 
-DATABASE = join(__file__, '../poc.db')
+DEFAULT_DB = '../poc.db'
+SCHEMA_PATH = '../schema.sql'
 
 
 def init_db(app):
+    """
+    Registers the database object in the context and the command in the cli
+    """
     app.teardown_appcontext(close_db)
+    app.cli.add_command(populate_db)
+
+
+@click.command('db:populate')
+@with_appcontext
+def populate_db():
+    """CLI command to populate empty databases. Database must exist."""
+    db = get_db()
+    with current_app.open_resource(SCHEMA_PATH, 'r') as f:
+        db.executescript(f.read())
+    db.commit()
+    click.echo('Database created.')
 
 
 def get_db():
+    """Opens a connection to the database and stores it in the global object"""
     if 'db' not in g:
-        g.db = sqlite3.connect(DATABASE)
+        # Get the database from the environment.
+        db_path = join(dirname(__file__),
+                       environ.get('DB', default=DEFAULT_DB))
+
+        if not exists(db_path):
+            raise FileNotFoundError(f'{db_path} does not exists')
+
+        # Initialise a connection and add it to the global object.
+        g.db = sqlite3.connect(db_path, detect_types=sqlite3.PARSE_DECLTYPES)
+
+        # Tells the connection to return rows that behave like dicts.
         g.db.row_factory = sqlite3.Row
 
     return g.db
 
 
 def close_db(exception=None):
+    """Closes the database connection when leaving the context"""
+    # Remove the reference from the global object.
     db = g.pop('db', None)
 
     if db is not None:
+        # Close the connection.
         db.close()
